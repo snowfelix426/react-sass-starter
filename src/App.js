6de9cbaeccf1d3c1bb58c6c1e1f4987fe447b849
snowfelix426 <felixleo426@gmail.com>
2021-09-11 04:54:17 -0400

    Clean project structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Header from './components/header'
+// import Footer from './components/footer'
+import HomePage from './pages/Home';
+import "./styles/index.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        {/* <Header /> */}
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+        {/* <Footer /> */}
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
